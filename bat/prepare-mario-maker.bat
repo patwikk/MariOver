69745cd7a5d798527ffddd60b1330338a939e314
patwikk <160638416+patwikk@users.noreply.github.com>
2026-06-09 11:17:58 -0500

    Added more .bat files from MarioDiffusion to here to get everything to work.
    Took more .bat files (evaluate_caption_adherence_multi.bat and run_diffusion_multi.bat) and tweaked them so they'd work here, also tweaking our existing batch files to work with them. Also needed to create a random captions script based off the one in MarioDiffusion for evaluate_caption_adherence_mutli.bat. Everything should run now though.

--- bat/prepare-mario-maker.bat
+++ bat/prepare-mario-maker.bat
@@ -23,3 +23,5 @@ set CAPTIONED_OUTPUT=datasets\%GAME%_LevelsAndCaptions-%TYPE%.json
 python build_dataset_with_ascii.py --input_file %INPUT% --output %RAW_OUTPUT% --tileset %TILESET% --include_all --sliding_window --stride 20
 python MarioMaker_create_ascii_captions.py --dataset %RAW_OUTPUT% --tileset %TILESET% --output %CAPTIONED_OUTPUT%
 python split_mario_maker_data.py --json %CAPTIONED_OUTPUT% --seed %SEED%
+python tokenizer.py save --json_file datasets\%GAME%_LevelsAndCaptions-%TYPE%-train.json --pkl_file datasets\%GAME%_Tokenizer-%TYPE%.pkl
+python create_mario_maker_random_captions.py --json %CAPTIONED_OUTPUT% --output datasets\%GAME%_RandomTest-%TYPE%.json
